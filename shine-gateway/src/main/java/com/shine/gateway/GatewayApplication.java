9e71a739ced0688231e1e0fffd3814b7197c8ae7
TJStep2018 <550871740@qq.com>
2022-04-26 22:37:10 +0800

    refactor:项目结构

---
+++ shine-gateway/src/main/java/com/shine/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.shine.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description:
+ * 
+ * @author: TJ
+ * @date:  2022-04-26
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
