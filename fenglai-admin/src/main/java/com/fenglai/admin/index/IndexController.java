87c7ec613b32ff576113ea2a4a8a3d28b9b21778
TJStep2018 <550871740@qq.com>
2022-05-08 22:08:51 +0800

    add:Graylog

--- fenglai-admin/src/main/java/com/fenglai/admin/index/IndexController.java
+++ fenglai-admin/src/main/java/com/fenglai/admin/index/IndexController.java
@@ -3,6 +3,7 @@ package com.fenglai.admin.index;
 import com.baomidou.mybatisplus.core.conditions.AbstractWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fenglai.common.web.response.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import java.util.Map;
  * @author: TJ
  * @date:  2022-04-26
  **/
+@Slf4j
 @RestController
 @RequestMapping("index")
 public class IndexController {
@@ -29,9 +31,9 @@ public class IndexController {
 
     @RequestMapping("/v1")
     public R get() {
-        String sql = "select * from data_test limit 1";
-        List<Map<String, Object>> maps = mysqlJdbc.queryForList(sql);
-
-        return R.ok(maps);
+//        String sql = "select * from data_test limit 1";
+//        List<Map<String, Object>> maps = mysqlJdbc.queryForList(sql);
+        log.info("卡死复健科");
+        return R.ok();
     }
 }
---
+++ fenglai-auth/src/main/java/com/fenglai/auth/AuthApplication.java
@@ -0,0 +1,9 @@
+package com.fenglai.auth;
+/**
+ * @description:
+ * 
+ * @author: TJ
+ * @date:  2022-05-06
+ **/
+public class AuthApplication {
+}
